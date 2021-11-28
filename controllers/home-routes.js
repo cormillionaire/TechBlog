83b83e45f3c477e536fa9921459e4e9d40832fe0
Cornelia Herman <corneliabherman@gmail.com>
2021-11-28 16:30:15 -0500

    not working yet but got a lot of the base in with some working api routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const { Blogpost } = require('../models');
+// Import the custom middleware
+const withAuth = require('../utils/auth');
+
+// GET all galleries for homepage
+router.get('/', async (req, res) => {
+  try {
+    const BlogpostData = await Blogpost.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name', 'description'],
+        },
+      ],
+    });
+
+    const blogposts = BlogpostData.map((blogpost) =>
+      blogpost.get({ plain: true })
+    );
+
+    res.render('homepage', {
+      blogposts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one gallery
+// Use the custom middleware before allowing the user to access the gallery
+
+// GET one painting
+// Use the custom middleware before allowing the user to access the painting
+// router.get('/painting/:id', withAuth, async (req, res) => {
+//   try {
+//     const dbPaintingData = await Painting.findByPk(req.params.id);
+
+//     const painting = dbPaintingData.get({ plain: true });
+
+//     res.render('painting', { painting, loggedIn: req.session.loggedIn });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
